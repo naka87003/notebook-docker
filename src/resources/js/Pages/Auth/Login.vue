10fff39bd3dc50b84eec5a280eecd87abf64e52f
naka87003 <naka87003@outlook.jp>
2024-08-07 18:46:39 +0900

    Login画面を作成

--- src/resources/js/Pages/Auth/Login.vue
+++ src/resources/js/Pages/Auth/Login.vue
@@ -1,71 +1,74 @@
-<script setup>
+<script setup lang="ts">
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import { Head, useForm } from '@inertiajs/vue3';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
-defineProps({
-    canResetPassword: {
-        type: Boolean,
-    },
-    status: {
-        type: String,
-    },
-});
+const props = defineProps<{
+  canResetPassword: boolean,
+  status: string
+}>();
 
 const form = useForm({
-    email: '',
-    password: '',
-    remember: false,
+  email: '',
+  password: '',
+  remember: false,
 });
 
 const visible = ref(false);
+const snackbar = ref(false);
+
+onMounted(() => {
+  if (props.status) {
+    snackbar.value = true;
+  }
+});
 
 const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
-    });
+  form.post(route('login'), {
+    onFinish: () => form.reset('password'),
+  });
 };
 </script>
 
 <template>
-    <GuestLayout>
+  <GuestLayout>
+    <v-snackbar v-model="snackbar" :timeout="3000" color="success" elevation="24" location="top right">
+      {{ status }}
+      <template v-slot:actions>
+        <v-btn variant="text" icon="mdi-close" @click="snackbar = false" />
+      </template>
+    </v-snackbar>
 
-        <Head title="Log in" />
-        <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
-            <div class="mx-auto text-success">
-                {{ status }}
-            </div>
-            <form @submit.prevent="submit">
-                <div class="text-subtitle-1 text-medium-emphasis">Email</div>
-                <v-text-field v-model="form.email" type="email" density="compact" placeholder="Email address"
-                    prepend-inner-icon="mdi-email-outline" variant="outlined" :error="form.errors.email"
-                    :error-messages="form.errors.email" required autofocus autocomplete="username"
-                    @input="form.errors.email = null"></v-text-field>
-
-                <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                    Password
-                    <a v-if="canResetPassword" :href="route('password.request')"
-                        class="text-caption text-decoration-none text-blue" rel="noopener noreferrer">
-                        Forgot login password?</a>
-                </div>
-                <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-                    :type="visible ? 'text' : 'password'" density="compact" placeholder="Enter your password"
-                    prepend-inner-icon="mdi-lock-outline" variant="outlined" v-model="form.password"
-                    :error="form.errors.password" :error-messages="form.errors.password"
-                    @click:append-inner="visible = !visible" autocomplete="current-password" required
-                    @input="form.errors.password = null"></v-text-field>
-                <v-btn color="blue" size="large" variant="tonal" :class="{ 'text-disabled': form.processing }"
-                    :disabled="form.processing" block @click="submit">
-                    Log In
-                </v-btn>
-                <v-checkbox color="info" name="remember" label="Remember me"
-                    v-model:checked="form.remember"></v-checkbox>
-                <v-card-text class="text-center">
-                    <a class="text-blue text-decoration-none" :href="route('register')" rel="noopener noreferrer">
-                        Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
-                    </a>
-                </v-card-text>
-            </form>
-        </v-card>
-    </GuestLayout>
+    <Head title="Log in" />
+    <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
+      <form @submit.prevent="submit">
+        <div class="text-subtitle-1 text-medium-emphasis">Email</div>
+        <v-text-field v-model="form.email" type="email" density="compact" placeholder="Email address"
+          prepend-inner-icon="mdi-email-outline" variant="outlined" :error="Boolean(form.errors.email)"
+          :error-messages="form.errors.email" required autofocus autocomplete="username"
+          @input="form.errors.email = null" />
+        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+          Password
+          <a v-if="canResetPassword" :href="route('password.request')"
+            class="text-caption text-decoration-none text-blue" rel="noopener noreferrer">
+            Forgot login password?</a>
+        </div>
+        <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
+          density="compact" placeholder="Enter your password" prepend-inner-icon="mdi-lock-outline" variant="outlined"
+          v-model="form.password" :error="Boolean(form.errors.password)" :error-messages="form.errors.password"
+          @click:append-inner="visible = !visible" autocomplete="current-password" required
+          @input="form.errors.password = null" />
+        <v-btn color="blue" size="large" variant="tonal" :class="{ 'text-disabled': form.processing }"
+          :disabled="form.processing" block @click="submit">
+          Log In
+        </v-btn>
+        <v-checkbox color="info" name="remember" label="Remember me" v-model:checked="form.remember"></v-checkbox>
+        <v-card-text class="text-center">
+          <a class="text-blue text-decoration-none" :href="route('register')" rel="noopener noreferrer">
+            Sign up now <v-icon icon="mdi-chevron-right" />
+          </a>
+        </v-card-text>
+      </form>
+    </v-card>
+  </GuestLayout>
 </template>
