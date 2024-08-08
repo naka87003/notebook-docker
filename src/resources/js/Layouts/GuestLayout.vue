a1e73dc1f3f89392811856e7a15ede4eadfffab9
naka87003 <naka87003@outlook.jp>
2024-08-08 18:28:40 +0900

    logoを暫定的にvuetifyに変更

--- src/resources/js/Layouts/GuestLayout.vue
+++ src/resources/js/Layouts/GuestLayout.vue
@@ -1,12 +1,10 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div>
-        <v-img class="mx-auto my-6" max-width="228"
-            src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"></v-img>
-        <slot />
-    </div>
+  <div>
+    <ApplicationLogo />
+    <slot />
+  </div>
 </template>
