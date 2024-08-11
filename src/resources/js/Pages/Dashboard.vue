bfa412d98f0e04c1fd730e4b0737918d568494ec
naka87003 <naka87003@outlook.jp>
2024-08-11 17:45:59 +0900

    mockをdbからの情報を表示させるように変更

--- src/resources/js/Pages/Dashboard.vue
+++ src/resources/js/Pages/Dashboard.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import dayjs from 'dayjs';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 
 const search = ref('');
+const notes = ref([]);
+
+onMounted((): void => {
+  loadNotes();
+});
+
+const loadNotes = (): void => {
+  axios.get(route('notes.index'))
+    .then(response => {
+      notes.value = response.data;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+const simplifyDateTime = (str: string): string => dayjs(str).format('YYYY/MM/DD HH:mm');
+const splitByNewline = (text: string): string[] => text.split(/\r?\n/);
 </script>
 
 <template>
@@ -20,140 +39,34 @@ const search = ref('');
     </template>
     <v-container>
       <v-row>
-        <v-col cols="12">
-          <v-card class="mx-auto" prepend-icon="mdi-note-text-outline" title="toast" rounded="0">
+        <v-col v-for="note in notes" cols="12">
+          <v-card :color="note.category.vuetify_theme_color_name" variant="tonal" class="mx-auto"
+            :prepend-icon="note.category.mdi_name" :title="note.title" rounded="0">
             <template v-slot:prepend>
               <v-icon size="large"></v-icon>
             </template>
             <template v-slot:append>
               <p class="text-caption">
-                2024/08/10 08:10
+                {{ simplifyDateTime(note.updated_at) }}
               </p>
             </template>
             <v-divider class="border-opacity-25 mx-1" />
             <v-card-text class="text-h6 py-2">
-              <p class="note-paragraph">不可算名詞</p>
-              <p class="note-paragraph">How may slices of toast do you have for breakfast?</p>
-            </v-card-text>
-            <v-card-actions>
-              <v-list-item class="w-100">
-                <template v-slot:prepend>
-                  <div class="justify-self-end">
-                    <v-icon color="#ffd700" size="small" class="me-1" icon="mdi-tag"></v-icon>
-                    <span class="me-5 text-caption">English</span>
-                    <v-icon size="small" class="me-1" icon="mdi-lock-outline"></v-icon>
-                  </div>
-                </template>
-                <template v-slot:append>
-                  <div class="justify-self-end">
-                    <v-icon size="small" class="me-5" icon="mdi-pencil-outline"></v-icon>
-                    <v-icon size="small" class="me-1" icon="mdi-archive-outline"></v-icon>
-                  </div>
-                </template>
-              </v-list-item>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-        <v-col cols="12">
-          <v-card color="primary" variant="tonal" class="mx-auto" prepend-icon="mdi-format-list-checks" title="買い物"
-            rounded="0">
-            <template v-slot:prepend>
-              <v-icon size="large"></v-icon>
-            </template>
-            <template v-slot:append>
-              <p class="text-caption">
-                2024/08/09 20:47
-              </p>
-            </template>
-            <v-divider class="border-opacity-25 mx-1" />
-            <v-card-text class="text-h6 py-2">
-              <p class="note-paragraph">豆腐</p>
-              <p class="note-paragraph">パン</p>
-              <p class="note-paragraph">ヨーグルト</p>
-            </v-card-text>
-            <v-card-actions>
-              <v-list-item class="w-100">
-                <template v-slot:prepend>
-                  <div class="justify-self-end">
-                    <v-icon size="small" class="me-1" icon="mdi-lock-outline"></v-icon>
-                  </div>
-                </template>
-                <template v-slot:append>
-                  <div class="justify-self-end">
-                    <v-icon size="small" class="me-5" icon="mdi-pencil-outline"></v-icon>
-                    <v-icon size="small" class="me-1" icon="mdi-archive-outline"></v-icon>
-                  </div>
-                </template>
-              </v-list-item>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-        <v-col cols="12">
-          <v-card class="mx-auto" prepend-icon="mdi-note-text-outline" title="" rounded="0">
-            <template v-slot:prepend>
-              <v-icon size="large"></v-icon>
-            </template>
-            <template v-slot:append>
-              <p class="text-caption">
-                2024/08/07 19:33
-              </p>
-            </template>
-            <v-divider class="border-opacity-25 mx-1" />
-            <v-card-text class="text-h6 py-2">
-              <p class="note-paragraph">
-                "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type
-                invalid as
-                well."
-              </p>
-            </v-card-text>
-            <v-card-actions>
-              <v-list-item class="w-100">
-                <template v-slot:prepend>
-                  <div class="justify-self-end">
-                    <v-icon color="#42B883" size="small" class="me-1" icon="mdi-tag"></v-icon>
-                    <span class="me-5 text-caption">Vue.js</span>
-                    <v-icon size="small" class="me-1" icon="mdi-heart-outline"></v-icon>
-                    <span class="me-5 text-caption">256</span>
-                  </div>
-                </template>
-                <template v-slot:append>
-                  <div class="justify-self-end">
-                    <v-icon size="small" class="me-5" icon="mdi-pencil-outline"></v-icon>
-                    <v-icon size="small" class="me-1" icon="mdi-archive-outline"></v-icon>
-                  </div>
-                </template>
-              </v-list-item>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-        <v-col cols="12">
-          <v-card color="secondary" variant="tonal" class="mx-auto" prepend-icon="mdi-calendar-clock" title="サウナ"
-            rounded="0">
-            <template v-slot:prepend>
-              <v-icon size="large"></v-icon>
-            </template>
-            <template v-slot:append>
-              <p class="text-caption">
-                2024/08/06 09:02
-              </p>
-            </template>
-            <v-divider class="border-opacity-25 mx-1" />
-            <v-card-text class="text-h6 py-2">
-              <v-alert type="info" variant="tonal" class="mb-3">
-                <p class="text-body-2">from 2024/08/11 Sun 08:15</p>
-                <p class="text-body-2">to 2024/08/11 Sun 14:00</p>
+              <v-alert v-if="note.category.id === 3" type="info" variant="tonal" class="mb-3">
+                <p class="text-body-2">from {{ simplifyDateTime(note.starts_at) }}</p>
+                <p class="text-body-2">to {{ simplifyDateTime(note.ends_at) }}</p>
               </v-alert>
-              <p class="note-paragraph">8:15 集合</p>
-              <p class="note-paragraph">8:30 白玉温泉</p>
-              <p class="note-paragraph">11:00 肉五郎（焼肉ランチ）</p>
-              <p class="note-paragraph">14:00 解散</p>
+              <p v-for="paragraph in splitByNewline(note.description)" class="note-paragraph">{{ paragraph }}</p>
             </v-card-text>
             <v-card-actions>
               <v-list-item class="w-100">
                 <template v-slot:prepend>
                   <div class="justify-self-end">
-                    <v-icon size="small" class="me-1" icon="mdi-heart-outline"></v-icon>
-                    <span class="me-5 text-caption">2</span>
+                    <template v-if="note.tag">
+                      <v-icon :color="note.tag?.hex_color" size="small" class="me-1" icon="mdi-tag"></v-icon>
+                      <span class="me-5 text-caption">{{ note.tag?.name }}</span>
+                    </template>
+                    <v-icon v-if="note.public === false" size="small" class="me-1" icon="mdi-lock-outline"></v-icon>
                   </div>
                 </template>
                 <template v-slot:append>
