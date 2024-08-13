a8949717a809e9d75e3d2dad50ecb944db7f33f3
naka87003 <naka87003@outlook.jp>
2024-08-13 18:24:52 +0900

    titleの前にiconを追加

--- src/resources/js/Components/TagCreateForm.vue
+++ src/resources/js/Components/TagCreateForm.vue
@@ -34,6 +34,9 @@ const submit = () => {
   <v-card>
     <v-toolbar density="comfortable" color="transparent">
       <v-toolbar-title class="text-h6" text="Create New Tag"></v-toolbar-title>
+      <template v-slot:prepend>
+        <v-icon class="ms-3" icon="mdi-tag-plus"/>
+      </template>
       <template v-slot:append>
         <v-btn icon="mdi-close" @click="$emit('close')"></v-btn>
       </template>
