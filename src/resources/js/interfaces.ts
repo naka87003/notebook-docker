a28e87947e3d36875a38200b5453bfe74d81066c
naka87003 <naka87003@outlook.jp>
2024-08-14 13:03:39 +0900

    typeを追加

--- src/resources/js/interfaces.ts
+++ src/resources/js/interfaces.ts
@@ -20,3 +20,31 @@ export type Note = {
     mdi_name?: string;
   }
 };
+
+export type Sort = {
+  key: string;
+  order: string;
+};
+
+export type Filter = {
+  category: number[],
+  tag: number[],
+  status: number
+};
+
+export type Category = {
+  id: number,
+  name: string,
+  mdi_name: string
+};
+
+export type Tag = {
+  id: number,
+  name: string,
+  hex_color?: string
+};
+
+export type Status = {
+  id: number,
+  name: string,
+};
