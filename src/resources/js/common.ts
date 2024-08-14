a02c9794ebfe7ff5249e5b5636a478ed1c8bab6a
naka87003 <naka87003@outlook.jp>
2024-08-14 13:03:14 +0900

    共通関数をまとめるcommon.jsを作成

---
+++ src/resources/js/common.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { Tag } from './interfaces';
+
+export const getTagSelectItems = async (): Promise<Tag[]> => {
+  const result = [];
+  await axios.get(route('tags.selectItems'))
+    .then(response => {
+      result.push(...response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  return result;
+};
