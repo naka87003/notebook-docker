46726b1a009eb143a99d644f903f8cf6b2b96225
naka87003 <naka87003@outlook.jp>
2024-08-14 17:47:27 +0900

    createは不要なので削除

--- src/app/Http/Controllers/NoteController.php
+++ src/app/Http/Controllers/NoteController.php
@@ -41,14 +41,6 @@ class NoteController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request): RedirectResponse
